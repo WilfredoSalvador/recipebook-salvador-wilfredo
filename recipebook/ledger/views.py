c7ff94f5c8b6de494b701495b911fecb52b7df06
Wilfredo Luis N. Salvador <wilfredo.salvador@student.ateneo.edu>
2025-03-11 20:17:41 +0800

    removed index function based view.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,13 +1,8 @@
 """Modules importing django views, HttpResponse and Recipe for url views."""
-from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
-
-def index(request):
-    """function printing homepage message."""
-    return HttpResponse('Check http://localhost:8000/recipes/list for the Recipe Book.')
 
 class RecipeList(ListView):
     """Class viewing the list of recipes."""
